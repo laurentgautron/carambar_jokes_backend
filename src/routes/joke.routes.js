d7b7d019e9b286acf6a6ba70e0f593b389239a23
laurentgautron <laurentgautron@MacBook-Air-de-laurentgautron.local>
2026-02-26 16:32:40 +0100

    first comment swagger

--- src/routes/joke.routes.js
+++ src/routes/joke.routes.js
@@ -3,7 +3,17 @@ import { Router } from "express";
 
 const router = Router();
 
+/**
+ * @swagger
+ * /jokes:
+ *   get:
+ *     summary: Récupère toutes les blagues
+ *     responses:
+ *       200:
+ *         description: Liste des blagues
+ */
 router.get('/', getAllJokes);
+
 router.get('/random', getRandomJoke);
 router.get('/:id', getOneJoke);
 router.post('/', addJoke);
